f914f6f74c7060c38ac2c78c375376abf9d43d94
loes01 <notreal@email.com>
2026-08-15 10:35:58 +0300

    getting system fully completed.

--- handlers/mode.hpp
+++ handlers/mode.hpp
@@ -8,11 +8,25 @@ class TextModeHandler {
             std::string inp;
             key = getch();
             while (key != '\n') {
-                inp.push_back(key);
+                if(inp.size() != 0 && key == KEY_BACKSPACE) {
+                    inp.pop_back();
+                }else {
+                    inp.push_back(key);
+                }
+                erase();
+                mvprintw(0, 0, "%s", inp.c_str());
+                refresh();
                 key = getch();
             }
+            printw("\n");
             //getting part <up>
 
             //execution part <down>
+            if(inp.starts_with("go ")) {
+                inp.erase(0, 3);
+                printw("%s", inp.c_str());
+                refresh();
+                getch();
+            }
         }
 };
